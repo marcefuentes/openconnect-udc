f05867bb599c57978cdae461ce611a0a32b1fb38
Marcelino Fuentes <marcelinofuentes@gmail.com>
2022-03-14 17:53:45 +0100

    Add comment

--- openconnect-udc.py
+++ openconnect-udc.py
@@ -8,6 +8,8 @@ import os
 os.system("sudo ifconfig tun0 down")
 host = "vpn.udc.es"
 driver = webdriver.Chrome("/usr/bin/chromedriver")
+#driver = webdriver.gecko.driver("/usr/bin/geckodriver")
+print('Ok')
 wait = WebDriverWait(driver, 60)
 driver.get("https://"+host)
 dsid = wait.until(lambda driver: driver.get_cookie("DSID"))
