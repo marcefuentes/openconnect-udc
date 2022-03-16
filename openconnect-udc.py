08b0d60d539a9bbf7e40df0d0732bad824162807
Marcelino Fuentes <marcelinofuentes@gmail.com>
2022-03-16 16:23:16 +0100

    Don't define a host

--- openconnect-udc.py
+++ openconnect-udc.py
@@ -6,10 +6,9 @@ import subprocess
 import os
 
 os.system("sudo ifconfig tun0 down")
-host = "vpn.udc.es"
+host = "put your host address here"
 driver = webdriver.Chrome("/usr/bin/chromedriver")
 #driver = webdriver.gecko.driver("/usr/bin/geckodriver")
-print('Ok')
 wait = WebDriverWait(driver, 60)
 driver.get("https://"+host)
 dsid = wait.until(lambda driver: driver.get_cookie("DSID"))
