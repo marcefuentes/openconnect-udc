b54be1c273f94dd2bdee97f99ca5fb9206a5c3f8
Marcelino Fuentes <marcelinofuentes@gmail.com>
2022-03-16 16:25:34 +0100

    Renamed

---
+++ openconnect-pulse.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+import subprocess
+import os
+
+os.system("sudo ifconfig tun0 down")
+host = "put your host address here"
+driver = webdriver.Chrome("/usr/bin/chromedriver")
+#driver = webdriver.gecko.driver("/usr/bin/geckodriver")
+wait = WebDriverWait(driver, 60)
+driver.get("https://"+host)
+dsid = wait.until(lambda driver: driver.get_cookie("DSID"))
+driver.quit()
+subprocess.run(["sudo", "/usr/sbin/openconnect", "-b", "-C", dsid["value"], "--protocol=pulse", host])
